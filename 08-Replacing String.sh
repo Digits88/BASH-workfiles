a3ef9c359b8abae52bf4cfd83cff97e250811a03
John Troony <ombagi@outlook.com>
2015-12-02 20:48:09 +0000

    Add Extra word (RedHat) on the string to modify

--- 08-Replacing String.sh
+++ 08-Replacing String.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Linux_distro="Debian RedHat Kali Ubuntu"
+Linux_distro="Debian RedHat Kali Ubuntu RedHat"
 
 echo "Our string is '$Linux_distro'"
 echo
@@ -19,5 +19,5 @@ echo ${Linux_distro#RedHat/Suse}
 echo
 
 echo "To replace  RedHat only if it's at the end of a string."
-echo ${Linux_distro%RedHat/Suse} will replace
+echo ${Linux_distro%RedHat/Suse}
 
